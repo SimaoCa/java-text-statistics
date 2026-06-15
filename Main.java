afe3ceb674beddfdbd2f3df53e1f4d1270b775d2
SimaoCa <simaocavalcante902@gmail.com>
2026-06-14 21:52:06 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,22 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Digite o texto: ");
-        Scanner ler = new Scanner(System.in);
-        String texto = ler.nextLine();
-        String [] palavras = texto.split(" ");
-        System.out.println("Numero de  palavras: "+ palavras.length);
-        int pares =0, impares= 0;
-        int quantidadeDeVogais = 0;
-       for (String p: palavras){
-           if (p.length() % 2 == 0) pares++;
+        String texto = String.join(" ", args); // junta todos os argumentos em um texto
+        String[] palavras = args;
+
+        int pares = 0, impares = 0, vogais = 0;
+
+        for (String p : palavras) {
+            if (p.length() % 2 == 0) pares++;
             else impares++;
-for (char c : p.toCharArray()) {
-    if ("aeiou".indexOf(c) != -1) {
-        quantidadeDeVogais++;
+
+            for (char c : p.toCharArray()) {
+                if ("aeiouAEIOU".indexOf(c) != -1) vogais++;
+            }
+        }
+
+        System.out.println("Numero de palavras: " + palavras.length);
+        System.out.println("Numero de vogais: " + vogais);
+        System.out.println("Palavras com letras impares: " + impares);
+        System.out.println("Palavras com letras pares: " + pares);
     }
-}
-       }
-        System.out.println("Numero de vogais: "+ quantidadeDeVogais);
-        System.out.println("Numero de palavras com a quantidade letras impares: "+ impares);
-        System.out.println("Numero de palvras com a quantidade de letras pares: "+ pares);
-     }
 }
